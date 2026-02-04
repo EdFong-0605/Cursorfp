9208648f23b9f43c87f78de34949321580113df3
eden <eden0fong065@gmail.com>
2026-02-03 21:18:26 -0800

    Enhance firebase.json configuration for better performance and clarity

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Dino! I created the hosting for the website!
         </p>
         <a
           className="App-link"
