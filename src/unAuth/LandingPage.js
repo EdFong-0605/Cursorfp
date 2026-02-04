8ab74adcb728d2c68c6c516a28a322448f5f44c7
eden <eden0fong065@gmail.com>
2026-02-03 21:18:21 -0800

    Refactor firebase.json configuration for improved settings

---
+++ src/unAuth/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <p className="landing-message">this is pretty hard yo</p>
+    </div>
+  );
+}
+
+export default LandingPage;
