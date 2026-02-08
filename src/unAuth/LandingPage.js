f5700aa9657b7a9abf3774d55be36dedbd1172bd
eden <eden0fong065@gmail.com>
2026-02-07 19:22:49 -0800

    frontend v1

--- src/unAuth/LandingPage.js
+++ src/unAuth/LandingPage.js
@@ -1,10 +1,18 @@
-import React from 'react';
 import './LandingPage.css';
 
+const RAINBOW = ['#e81416', '#ffa500', '#faeb36', '#79c314', '#487de7', '#4b369d', '#70369d'];
+
 function LandingPage() {
+  const text = 'This is kinda hard yo';
   return (
     <div className="landing-page">
-      <p className="landing-message">this is pretty hard yo</p>
+      <h1 className="rainbow-text">
+        {text.split('').map((char, i) => (
+          <span key={i} style={{ color: RAINBOW[i % RAINBOW.length] }}>
+            {char === ' ' ? '\u00A0' : char}
+          </span>
+        ))}
+      </h1>
     </div>
   );
 }
